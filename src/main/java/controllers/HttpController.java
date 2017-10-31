52a93c60186f2819fedb85425a0ab03567de2f46
tobymohr <tobiasmohr22@gmail.com>
2017-10-31 14:26:09 +0100

    added constants

--- src/main/java/controllers/HttpController.java
+++ src/main/java/controllers/HttpController.java
@@ -2,18 +2,16 @@ package controllers;
 
 
 import org.springframework.http.*;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.xml.ws.Response;
 import java.util.Arrays;
-import java.util.concurrent.Future;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static tools.Constants.remote_search_base_url;
 
 @RestController
 public class HttpController {
@@ -22,7 +20,7 @@ public class HttpController {
 
     @RequestMapping(value = base_url + "searchitems", method = GET)
     public String elasticSearchItems(@RequestParam String query, @RequestParam(value="type", defaultValue="") String type) {
-        final String uri = "http://localhost:8081/items/searchitems";
+        final String uri = remote_search_base_url +"/items/searchitems";
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri)
                 .queryParam("query", query);
         if(!type.isEmpty()){
@@ -32,12 +30,13 @@ public class HttpController {
         return result.getBody();
     }
 
+
+
     private ResponseEntity<String> getUrl(   UriComponentsBuilder builder){
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-        System.out.println(builder.build().encode().toUri());
         return restTemplate.exchange(builder.build().encode().toUri(), HttpMethod.GET, entity, String.class);
     }
 
